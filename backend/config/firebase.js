c6530743c36f547dcb8385c93431ffff2d83232c
Earl Mesa <earl.rustein.mesa@gmail.com>
2024-12-28 21:56:10 +0800

    Fix config

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+const serviceAccount = Buffer.from(process.env.FIREBASE_KEY_JSON, 'base64').toString('utf8');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
