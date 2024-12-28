c2fcd33e56e9f23e99d4eb2f77b96e4b14b5662c
Earl Mesa <earl.rustein.mesa@gmail.com>
2024-12-28 22:04:51 +0800

    Fix config issue

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = Buffer.from(process.env.FIREBASE_KEY_JSON, 'base64').toString('utf8');
+const serviceAccount = JSON.parse(Buffer.from(process.env.FIREBASE_KEY, 'base64').toString('utf8'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
 const emailRoutes = require('./routes/emailRoutes');
 require('./utils/cronJobs');
 
