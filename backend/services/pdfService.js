977c08334912d61885bdadd7254545914ce2d85e
Earl Mesa <earl.rustein.mesa@gmail.com>
2024-12-29 18:36:53 +0800

    Test fix

--- backend/services/pdfService.js
+++ backend/services/pdfService.js
@@ -4,6 +4,7 @@ const { generateHTMLContent } = require('./../templates/generated-html');
 const generatePDF = async (data) => {
   const { salesData, totalGrossSales, expensesData, totalExpensesData, dateRange } = data;
 
+  console.log('======================Executable Path:', puppeteer.executablePath());
   const browser = await puppeteer.launch({ headless: 'new' });
   const page = await browser.newPage();
 
